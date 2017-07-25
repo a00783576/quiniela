26cc9be40f096af3a7f50a10eb316458ed736ef3
Arturo Gutierrez <arturo-gutierrez@live.com>
2017-07-25 15:45:55 -0500

    Testing Web Hooks!!

--- src/Home.tsx
+++ src/Home.tsx
@@ -8,7 +8,7 @@ export default class Home extends React.Component<object, object> {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text>A toooooooooooodos nosotrooooooos</Text>
+        <Text>A toooooooooooodos ustedeeeeeeeee</Text>
       </View>
     );
   }
